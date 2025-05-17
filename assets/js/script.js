269862622dd831aa36d3c41afe95b4110d13f844
Nguyen The Hiep <169110383+nthiep14082005@users.noreply.github.com>
2025-05-17 22:44:14 +0700

    UPDATE new contact link

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,31 @@ function showSlides() {
 setInterval(showSlides, 8000); 
 
 
+// // Countdown timer for 3 hours in .sale-slogan.slg1
+// const countdownElement = document.querySelector('.sale-slogan.slg1');
+// let countdownTime = 3 * 60 * 60; // 3 hours in seconds
 
+// function updateCountdown() {
+//     if (!countdownElement) return;
+//     let hours = Math.floor(countdownTime / 3600);
+//     let minutes = Math.floor((countdownTime % 3600) / 60);
+//     let seconds = countdownTime % 60;
+
+//     countdownElement.textContent = 
+//         `SALE ENDS AFTER : ${hours.toString().padStart(2, '0')}:` +
+//         `${minutes.toString().padStart(2, '0')}:` +
+//         `${seconds.toString().padStart(2, '0')}`;
+
+//     if (countdownTime > 0) {
+//         countdownTime--;
+//     } else {
+//         clearInterval(timerInterval);
+//         countdownElement.textContent = "END!";
+//     }
+// }
+
+// const timerInterval = setInterval(updateCountdown, 1000);
+// updateCountdown();
 
 
 
